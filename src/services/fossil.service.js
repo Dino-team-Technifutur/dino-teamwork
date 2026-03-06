df43e633fc0066a4d45ddfe57e526078dcf062f2
nathan-faucher <nathan.faaucher@gmail.com>
2026-03-06 15:07:06 +0100

    [ADD] fossil.service

---
+++ src/services/fossil.service.js
@@ -0,0 +1,28 @@
+import db from '../database/index.js'
+import {InvalidfossilError} from '../custom-errors/fossil.error.js';
+
+const fossilService = {
+    getAll: async () => {
+        return await db.Fossil.findAll();
+    },
+    create: async(data) => {
+        const existingfossil = await db.Fossil.findOne
+        ({
+            where:{
+                name: data.name
+            },
+            })
+            if (existingfossil){
+                throw new InvalidfossilError();
+            }  
+    },
+  delete : async (fossilId) => {
+    const fossil = await db.Fossil.findByPk(fossilId);
+    if (!fossil) {
+      throw new InvalidFossilError();
+    }
+    await db.Fossil.destroy(fossilId);
+  }
+};
+
+export default fossilService;
