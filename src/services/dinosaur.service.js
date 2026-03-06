784b00ba4fe6ed429d44daebcb91a426939fa04c
Johan Godefroid <johangodefroid@MacBook-Pro-de-Johan.local>
2026-03-06 14:32:42 +0100

    Fix Service Create

--- src/services/dinosaur.service.js
+++ src/services/dinosaur.service.js
@@ -4,6 +4,17 @@ const dinosaurService = {
     getAll: async () => {
         return await db.Dinosaur.findAll();
     },
+    create: async(data) => {
+        const existingDinosaur = await db.Dinosaur.findOne
+        ({
+            where:{
+                name: data.name
+            },
+            })
+            if (existingDinosaur){
+                throw new InvalidDinosaurError();
+            }  
+    },
 };
 
 export default dinosaurService;
