237a00090a134029f43b58eccd15ef464df8e3f2
nathan-faucher <nathan.faaucher@gmail.com>
2026-03-06 13:38:51 +0100

    added fossil to dino relation

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,15 @@ import Fossil from './entities/fossil.entity.js'
 import Species from './entities/species.entity.js';
 import Zone from './entities/zone.entity.js';
 
+Fossil.belongsTo(Dinosaur, {
+  as: "dinosaur",
+  foreignKey: {
+    allowNull: true,
+    name: "dinosaurId",
+  },
+});
 
-
-export {
+export default {
   Dinosaur,
   Fossil,
   Species,
