4ffcb87ed1a8af3c1d77a2c031c8c22bf67e949b
Johan Godefroid <johangodefroid@MacBook-Pro-de-Johan.local>
2026-03-06 13:49:09 +0100

    index.js modif

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,14 @@ import Fossil from './entities/fossil.entity.js'
 import Species from './entities/species.entity.js';
 import Zone from './entities/zone.entity.js';
 
+// ZONE/SPECIES
 
+Zone.belongsToMany(Species, {
+    as: "Species",
+    through: "species_zones",
+    foreignKey: "ZoneId",
+    otherKey: "SpeciesId",
+});
 
 export {
   Dinosaur,
